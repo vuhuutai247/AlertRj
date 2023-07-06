ddbb6bb167dae6d62e1acbc8101de321b8a156f9
vuhuutai247 <vuhuutai1@gmail.com>
2023-07-06 22:55:32 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Alert from './Components/Alert';
+
+function App() {
+  return (
+   <Alert/>
+  );
+}
+
+export default App;
---
+++ src/Components/Alert.jsx
@@ -0,0 +1,9 @@
+const Alert = () =>{
+    return(
+        <div class="alert alert-warning" role="alert">
+  Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại.
+        </div>
+    )
+}
+
+export default Alert
